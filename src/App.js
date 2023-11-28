bfb7d4da923f9c9d630ea881ef26c1566a8ee6c1
Bishal487 <bishal.kunwar487@gmail.com>
2023-11-28 12:48:26 -0500

    into the login ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import Home from "./components/Home"
+import Home from "./container/Home";
+import Login from "./components/Login";
+import {Routes, Route, useNavigate} from "react-router-dom";
 
 export default function App(){
   return(
-    <div className="app">
-      <Home/>
-    </div>
+    <Routes>
+      <Route path="login" element={<Login/>} />
+      <Route path="/*" element={<Home/>} />
+    </Routes>
   )
 }
